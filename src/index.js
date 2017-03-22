89c8a485931b82b026c1dc47c746ff8411ff84b1
Adli Fariz Bonaputra <abathz@gmail.com>
2017-03-23 01:06:52 +0700

    first init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+   return(
+      <div>React, Redux and Webpack Boilerplate</div>
+   );
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
