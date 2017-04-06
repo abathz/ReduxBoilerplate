184fe86835c59dcbf6675ffb11e67a0e96550be9
Adli Fariz Bonaputra <abathz@gmail.com>
2017-04-06 18:52:31 +0700

    set reducers

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
    render() {
       return(
          <div className="text-center">
-            <h1>React, Redux and Webpack Boilerplate</h1>
+            <h1>Welcome to Redux Boilerplate</h1>
          </div>
       );
    }
