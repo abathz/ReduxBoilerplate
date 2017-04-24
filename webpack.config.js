c80d1ddc8b7bb159595187067fc0cadce22dd1c3
Adli Fariz Bonaputra <abathz@gmail.com>
2017-04-24 20:56:55 +0700

    refactor webpack dev and webpack prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
+const path = require('path');
+
 module.exports = {
-   entry: [
-      './src/index.js'
-   ],
-   output: {
-      path: __dirname,
-      publicPath: '/',
-      filename: 'bundle.js'
-   },
-   module: {
-      loaders: [{
-         exclude: /node_modules/,
-         loader: 'babel',
-         query: {
-            presets: ['react', 'es2015', 'stage-1']
-         }
-      }]
-   },
-   resolve: {
-      extensions: ['', '.js', '.jsx']
-   },
-   devServer: {
-      historyApiFallback: true,
-      contentBase: './'
-   }
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      components: path.resolve(__dirname, 'src/components/'),
+      actions: path.resolve(__dirname, 'src/actions/'),
+      reducers: path.resolve(__dirname, 'src/reducers/')
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
 };
