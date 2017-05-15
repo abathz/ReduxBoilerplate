1719e0f3231d978086dec964391d338b2c0f4637
Adli Fariz Bonaputra <abathz@gmail.com>
2017-05-15 18:38:53 +0700

    refactor render to #main, move container class to app component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,7 +3,7 @@ import { Component } from 'react';
 export default class App extends Component {
 	render() {
 		return(
-			<div className="text-center">
+			<div className="container text-center">
 				<h1>Welcome to Redux Boilerplate</h1>
 			</div>
 		);
